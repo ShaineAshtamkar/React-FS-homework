56387df3c113caaaa06b3cf7e6998c15669231a9
Shaine <shaineashtamkar099@gmail.com>
2026-02-10 20:32:01 +0200

    Render QuickNotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./ReactDataFlow/App.jsx";
+import App from "./QuickNotes/App.jsx";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>
+
 );
